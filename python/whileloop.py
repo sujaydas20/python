07be4d1d7dea3f5e6ec6893fb4e74162832948c9
sujaydas <sujaydas17595@gmail.com>
2026-07-06 22:10:32 +0530

    while loop

---
+++ python/whileloop.py
@@ -0,0 +1,24 @@
+# while loop define that loop is perform until the condition is not false
+
+
+
+i=1 
+while i<11:
+    print(i)
+    i=i+1
+
+
+
+a=int(input("enter your number="))
+while a in [1,2,3,6,5]:
+    print("you won the game\nyour number is = ",a)
+    break
+
+
+
+
+#  infinte loop
+# i=1
+# while i<4:
+#     print(i+1)
+    
