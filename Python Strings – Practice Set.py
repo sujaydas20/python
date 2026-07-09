d9b2b3dc9dc236c833b21197dd10236411e6e4c2
sujaydas <sujaydas17595@gmail.com>
2026-07-09 11:49:16 +0530

    strings

---
+++ Python Strings – Practice Set.py
@@ -0,0 +1,36 @@
+string="sujay"
+print(len(string))
+print(string[0])
+print(string[-1])
+
+
+
+
+text="sujay is a coder"
+print(text[0:6])
+print(text[-6])
+print(text[::2])
+
+
+
+
+text="sujay is a coder"
+print(text[::-1])
+
+
+
+text="  sujay is a coder "
+print(text.strip())
+print(text.title())
+print(text.count("o"))
+
+
+
+str1="1234abcd"
+print(str1.isalnum())
+
+
+
+name="sujay"
+age=25
+print(f"my name is {name} an i am {age} year old")
---
+++ slicing.py
@@ -0,0 +1,6 @@
+text = "Hello, Python!"
+print(text[0:5])   # Output: Hello
+print(text[:5])    # Output: Hello (same as text[0:5])
+print(text[7:])    # Output: Python! (from index 7 to end)
+print(text[::2])   # Output: Hlo Pto!
+print(text[-6:-1]) # Output: ython (negative indexing)
