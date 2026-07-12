bc3c51097b8ec01082bfd6268159b956285df0f2
sujaydas <sujaydas17595@gmail.com>
2026-07-12 23:28:01 +0530

    Update lamdafunction.py
    factorial

--- lamdafunction.py
+++ lamdafunction.py
@@ -1,5 +1,13 @@
-sum=lambda x,y:x*y
-x=int(input("enter your number"))
-y=int(input("enter your number"))
+# sum=lambda x,y:x*y
+# x=int(input("enter your number"))
+# y=int(input("enter your number"))
 
-print (sum(x,y))
+# print (sum(x,y))
+
+def factorial(n):
+    fact = 1
+    for i in range(1, n + 1):
+        fact *= i
+    return fact
+
+print(factorial(5))
