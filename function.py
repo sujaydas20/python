54c60ea8c49a0665bf1b89f8d02af331115566ae
sujaydas <sujaydas17595@gmail.com>
2026-07-13 23:34:20 +0530

    finding the 2nd largest number

--- function.py
+++ function.py
@@ -1,26 +1,36 @@
 def avg_(a,b):
     d=(a+b)/2
     print(d)
-avg_(2,3)    
+# avg_(2,3)    
 
 
-def agv(a,c):
-    d=(a+c)*9
-    return d
-o1=agv(1,2,)
-o2=agv(2,5,)
+# def agv(a,c):
+#     d=(a+c)*9
+#     return d
+# o1=agv(1,2,)
+# o2=agv(2,5,)
 
 
-print(o1)
-print(o2)
+# print(o1)
+# print(o2)
 
 
 
 
-def value_(a=int(input("enter your number= ")),b=int(input("enter ypur number= "))):
-    d=(a*b)
-    return d
+# def value_(a=int(input("enter your number= ")),b=int(input("enter ypur number= "))):
+#     d=(a*b)
+#     return d
 
-c =value_()
-print(c)
-# c1=value_(a,b=)
+# c =value_()
+# print(c)
+# # c1=value_(a,b=)
+
+
+
+
+def number_(num):
+    value=list(set(num))
+    value.sort()
+    return value[-2]
+num=[1,2,5,6,4]
+print (number_(num))
