83c925c37c32555ac1fc154a13dd1d0473ced212
sujaydas <sujaydas17595@gmail.com>
2026-07-18 00:21:30 +0530

    check the maximum number useing the function an dosc string

--- docs string .py
+++ docs string .py
@@ -4,3 +4,26 @@ def sum(a,b):
     return c
 print(sum(4,5))
 print(sum.__doc__)
+
+
+
+# check the maxmum vlaue useing function and docs string
+
+
+
+def maximum(a, b):
+    """This function checks which number is greater."""
+
+    if a >= b:
+        return "a is the greater number"
+    else:
+        return "b is the greater number"
+
+
+a = int(input("Enter first value: "))
+b = int(input("Enter second value: "))
+
+print(maximum(a, b))
+print(maximum.__doc__)
+
+   
