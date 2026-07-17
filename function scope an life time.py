23483ef18a31087799772e15520f1d2e9f0f7160
sujaydas <sujaydas17595@gmail.com>
2026-07-17 23:52:16 +0530

    global veriable

--- function scope an life time.py
+++ function scope an life time.py
@@ -1,5 +1,31 @@
 def sum (a,b): 
     # a na b are the local veriable
     c=a+b  
+    z=1# it is a local veriable
     return c
+def greet():
+    z=25
+    print("hello")
+
+z=8#it is a golbal veriable
+greet ()
 print(sum(8,9))
+print(z)
+
+
+
+
+
+
+
+
+# global veriable
+def sum(a,b):
+    print("hey i am sujay")
+    c=a+b
+    global z
+    z=0
+    return c
+z=3
+print(sum(7,3))
+print(z)
