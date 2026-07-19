4e0c220f86b7b6600be796a54d936537f1c6e6a9
sujaydas <sujaydas17595@gmail.com>
2026-07-19 20:49:38 +0530

    area of a rectangle

--- Practice Set Python Functions & Modules.py
+++ Practice Set Python Functions & Modules.py
@@ -1,15 +1,22 @@
-def preent():
-    print("hello python learner")
-preent()    
+# def preent():
+#     print("hello python learner")
+# preent()    
 
 
 
-def squer(x):
-    return (x*x)
-print(squer(4))
+# def squer(x):
+#     return (x*x)
+# print(squer(4))
 
 
 
-def name (frist,second):
-    return f"{frist}{second}"
-print(name("sujay","das"))
+# def name (frist,second):
+#     return f"{frist}{second}"
+# print(name("sujay","das"))
+
+
+
+# check the area of the rectangle
+def area(lenth,breath):
+    return lenth*breath
+print(area(70,80))
