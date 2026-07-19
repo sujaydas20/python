81ad3d1024d11138ceb986f83e66cc588238954e
sujaydas <sujaydas17595@gmail.com>
2026-07-20 00:01:15 +0530

    Update Practice Set Python Functions & Modules.py

--- Practice Set Python Functions & Modules.py
+++ Practice Set Python Functions & Modules.py
@@ -1,18 +1,18 @@
-# def preent():
-#     print("hello python learner")
-# preent()    
+def preent():
+    print("hello python learner")
+preent()    
 
 
 
-# def squer(x):
-#     return (x*x)
-# print(squer(4))
+def squer(x):
+    return (x*x)
+print(squer(4))
 
 
 
-# def name (frist,second):
-#     return f"{frist}{second}"
-# print(name("sujay","das"))
+def name (frist,second):
+    return f"{frist}{second}"
+print(name("sujay","das"))
 
 
 
@@ -20,3 +20,18 @@
 def area(lenth,breath):
     return lenth*breath
 print(f"the area of a rectangle is {area(70,80)}")
+
+
+
+
+#lamda function
+
+
+add= lambda a,b:a+b
+
+print(add(5,5))
+
+
+squere=lambda x:x*x
+lst=[1,2,3,4]
+print((map(squere,lst)))
