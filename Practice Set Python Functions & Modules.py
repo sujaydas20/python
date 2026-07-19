4634314710fe60152f240600e773bacd5104473b
sujaydas <sujaydas17595@gmail.com>
2026-07-19 13:38:03 +0530

    practice set

---
+++ Practice Set Python Functions & Modules.py
@@ -0,0 +1,15 @@
+def preent():
+    print("hello python learner")
+preent()    
+
+
+
+def squer(x):
+    return (x*x)
+print(squer(4))
+
+
+
+def name (frist,second):
+    return f"{frist}{second}"
+print(name("sujay","das"))
