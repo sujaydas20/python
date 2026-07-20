da02ff2a58198e7a7489be90eab021767e260c45
sujaydas <sujaydas17595@gmail.com>
2026-07-20 23:59:46 +0530

    Create listmethod.py

---
+++ listmethod.py
@@ -0,0 +1,16 @@
+lst=[1,3,44,5,66,77,7,99,00]
+print(lst)
+lst.append(55)
+print(lst)
+lst.pop(8)
+print(lst)
+lst.sort()
+print(lst)
+
+
+def lst(num):
+    num= [1, 4, 5, 6, 88, 66, 99, 0]
+    return lst(num)
+
+print(list(lst))
+
