ec6262e450dc62c1dc6a2322d8c065c9af8bd8e7
sujaydas <sujaydas17595@gmail.com>
2026-07-22 00:53:58 +0530

    dictionary comprehensions in python
    '''List	Ordered, Mutable	Storing sequences, dynamic data
    Tuple	Ordered, Immutable	Fixed collections, dictionary keys
    Set	Unordered, Unique	Removing duplicates, set operations
    Dictionary	Key-Value Pairs	Fast lookups, structured data'''

---
+++ Dictionary Comprehensions.py
@@ -0,0 +1,8 @@
+table={i:5*i for i in range(1,11)}
+print(table)
+
+
+'''List	Ordered, Mutable	Storing sequences, dynamic data
+Tuple	Ordered, Immutable	Fixed collections, dictionary keys
+Set	Unordered, Unique	Removing duplicates, set operations
+Dictionary	Key-Value Pairs	Fast lookups, structured data'''
