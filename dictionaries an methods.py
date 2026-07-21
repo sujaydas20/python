09329d0b64fd8309d4fda2a2e5a9adb0463b3e5e
sujaydas <sujaydas17595@gmail.com>
2026-07-22 00:42:59 +0530

    different type to use the dic

--- dictionaries an methods.py
+++ dictionaries an methods.py
@@ -2,9 +2,44 @@ sutend={"name":"sujay","age":"21","college":"pce nagpur"}
 print(sutend,type(sutend))
 
 
-mark={'sujay':99,'sumit':73,'rajat':'fail'}
-print(mark['rajat'])
 
 
-stduent={"name":(input("enter your name "))}
-print(stduent)
+
+mark = {
+    'sujay': int(input("Enter Sujay's marks: ")),
+    'sumit': int(input("Enter Sumit's marks: ")),
+    'rajat': int(input("Enter Rajat's marks: "))
+}
+
+if mark["sujay"] >= 35:
+    print("Sujay: Pass")
+else:
+    print("Sujay: Fail")
+
+if mark["sumit"] >= 35:
+    print("Sumit: Pass")
+else:
+    print("Sumit: Fail")
+
+if mark["rajat"] >= 35:
+    print("Rajat: Pass")
+else:
+    print("Rajat: Fail")
+
+
+
+mark = {
+    'sujay': int(input("Enter Sujay's marks: ")),
+    'sumit': int(input("Enter Sumit's marks: ")),
+    'rajat': int(input("Enter Rajat's marks: "))
+}
+
+for name, score in mark.items():
+    if score >= 35:
+        print(name, "Pass")
+    else:
+        print(name, "Fail")
+
+
+
+
