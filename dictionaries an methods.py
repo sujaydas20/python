f5e1d1af399dbe58e23bdac79271c19c38b027f9
sujaydas <sujaydas17595@gmail.com>
2026-07-22 00:23:21 +0530

    dictionaries in python

---
+++ dictionaries an methods.py
@@ -0,0 +1,10 @@
+sutend={"name":"sujay","age":"21","college":"pce nagpur"}
+print(sutend,type(sutend))
+
+
+mark={'sujay':99,'sumit':73,'rajat':'fail'}
+print(mark['rajat'])
+
+
+stduent={"name":(input("enter your name "))}
+print(stduent)
