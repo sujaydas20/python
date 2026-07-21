f83286c1fe785e5ced03db399b69098ac647f916
sujaydas <sujaydas17595@gmail.com>
2026-07-21 23:35:27 +0530

    sets and method

---
+++ set an method.py
@@ -0,0 +1,16 @@
+# s={"11",'55',"66","8","1"}
+s={1,5,6,8,9}
+print(s,type(s))
+s.remove(1)
+s.add(10)
+print(s)
+
+
+
+
+a={1,5,9,3,4,7}
+b={1,5,9,3,7,4,2}
+c=a.union(b)
+print(c)
+d=a.intersection(b)
+print(d)
