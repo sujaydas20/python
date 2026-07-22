b3486300b5f6f009ad4ceae9c4eb97c33873e736
sujaydas <sujaydas17595@gmail.com>
2026-07-23 00:03:02 +0530

    slicing using list in python

--- datastucture_practiceset.py
+++ datastucture_practiceset.py
@@ -3,3 +3,13 @@ print(frutes[0])
 frutes[1]='orange'
 print(len(frutes))
 print(frutes)
+
+
+
+
+lis=[i for i in range(1,11)]
+
+lis=[1,2,3,4,5,6,7,8,9,10]
+print(lis)
+print (lis[0:3])
+print(lis[-3:])
