6a71e4657116f1189e2873e2bb2546004bbeb08d
sujaydas <sujaydas17595@gmail.com>
2026-07-26 16:48:46 +0530

    class in python

--- opps.py
+++ opps.py
@@ -1,4 +1,6 @@
 """
+class
+
 Abstraction:
 Encapsulation:
 Inheritance:
@@ -7,3 +9,16 @@ Polymorphism:
 
 
 # class an object
+"""
+Class: Think of a class as a blueprint or a template. It defines what an object will be like – what data it will hold and what actions it can perform. It doesn't create the object itself, just the instructions for creating it. It's like an architectural plan for a house.
+
+Object (Instance): An object is a specific instance created from the class blueprint. If "Car" is the class, then your red Honda Civic is an object (an instance) of the "Car" class. Each object has its own unique set of data. It's like the actual house built from the architectural plan.
+
+"""
+class employe:
+    company='asus'
+    def get_salary(self):
+        return 40000
+e=employe()
+print(e.get_salary())
+print(e.company)
