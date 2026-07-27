96504ca7b4018bef73bffff2aa0bed533e195c0e
sujaydas <sujaydas17595@gmail.com>
2026-07-28 00:04:07 +0530

    inheritance in python

---
+++ inheritance.py
@@ -0,0 +1,18 @@
+
+
+
+class animal:
+    location='india'
+    def __init__(self,name):
+        self.name=name
+    def speak(self):
+        print("animal sound")
+class dog(animal):
+    def speak(self):
+        # super().speak()
+        print("woof")
+
+d= dog("bruno")
+# a=animal("dog")
+d.speak()            
+# print(d.location)
