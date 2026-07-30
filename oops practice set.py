96bd9660cb7c2004dca40867dc7a06b57946a864
sujaydas <sujaydas17595@gmail.com>
2026-07-31 00:05:27 +0530

    Update oops practice set.py

--- oops practice set.py
+++ oops practice set.py
@@ -3,3 +3,15 @@ class car:
         print("car is moveing")
 car1=car()
 car1.drive()
+
+
+
+
+# construction
+class  person:
+    def __init__(self,name,age):
+
+        self.name=name
+        self.age=age
+p1=person('sujay',21)
+print(p1.name,p1.age)
