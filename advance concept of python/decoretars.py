86582c013d8545d880a1a19dac052c82de67acb9
sujaydas <sujaydas17595@gmail.com>
2026-08-01 06:47:57 +0530

    @decoraters  call function

--- advance concept of python/decoretars.py
+++ advance concept of python/decoretars.py
@@ -3,10 +3,22 @@ def decoraters(func):
         print("i an about to excuted a function.........")
         func ()
         print("i have excute this finction............")
-    return warpper     
-
+    return warpper
+     
+@decoraters
 def say_hello():
     print("hello !")
-# say_hello()    
-f=decoraters(say_hello)
-f()
+say_hello()    
+# f=decoraters(say_hello)
+# f()
+
+
+"""
+f will look like this def f()
+def f()
+    print("i am about to excuted a functoin.....")
+    print("hello")
+    print("i have executed this function..........")
+
+
+"""
