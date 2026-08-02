0e41ec98022e211fc25cff46ac9b0d6e5181101d
sujaydas <sujaydas17595@gmail.com>
2026-08-02 23:57:53 +0530

    Using @property (Pythonic Approach)

--- advance concept of python/Getters and Setters.py
+++ advance concept of python/Getters and Setters.py
@@ -5,7 +5,44 @@ class employe:
 
     def frist_name(self):
         l=self.name.split(" ")
-        print(l)
+        # print(l)
         return l[0]
 e= employe("sujay das",10000)
 print(e.frist_name())    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Using @property (Pythonic Approach)
+
+class Person:
+    def __init__(self, name):
+        self._name = name
+
+    @property
+    def name(self):  # Getter
+        return self._name
+
+    @name.setter
+    def name(self, new_name):  # Setter
+        self._name = new_name 
+
+p = Person("Alice")
+print(p.name)  # Alice (calls the getter)
+
+p.name = "Bob"  # Calls the setter
+print(p.name)  # Bob
+ 
