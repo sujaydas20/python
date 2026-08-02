1a2528a4ae649a66cf9640cd51d1b1459dead36f
sujaydas <sujaydas17595@gmail.com>
2026-08-02 23:58:40 +0530

    class Circle:     def __init__(self, radius):         self._radius = radius      @property     def radius(self):         return self._radius      @property     def area(self):  # Read-only computed property         return 3.1416 * self._radius * self._radius  c = Circle(5) print(c.radius)  # 5 print(c.area)  # 78.54  # c.radius = 10  # Raises AttributeError: can't set attribute # c.area = 20 # Raises AttributeError: can't set attribute

--- advance concept of python/Getters and Setters.py
+++ advance concept of python/Getters and Setters.py
@@ -45,4 +45,33 @@ print(p.name)  # Alice (calls the getter)
 
 p.name = "Bob"  # Calls the setter
 print(p.name)  # Bob
- 
+
+
+
+#  Read-Only Properties
+
+
+
+
+
+
+
+
+class Circle:
+    def __init__(self, radius):
+        self._radius = radius
+
+    @property
+    def radius(self):
+        return self._radius
+
+    @property
+    def area(self):  # Read-only computed property
+        return 3.1416 * self._radius * self._radius
+
+c = Circle(5)
+print(c.radius)  # 5
+print(c.area)  # 78.54
+
+# c.radius = 10  # Raises AttributeError: can't set attribute
+# c.area = 20 # Raises AttributeError: can't set attribute
