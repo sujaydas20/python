8e3aa8b8b30e437dd3c136327ce2a06e3ff3d45b
sujaydas <sujaydas17595@gmail.com>
2026-08-03 22:50:16 +0530

    Update Getters and Setters.py

--- advance concept of python/Getters and Setters.py
+++ advance concept of python/Getters and Setters.py
@@ -16,6 +16,28 @@ print(f"the author of the algoritham is sujay das")
 
 
 
+class Person:
+    def __init__(self, name):
+        self._name = name
+
+    @property
+    def name(self):  # Getter
+        return self._name
+
+    @name.setter
+    def name(self, new_name):  # Setter
+        self._name = new_name 
+
+p = Person("Alice")
+print(p.name)  # Alice (calls the getter)
+
+p.name = "Bob"  # Calls the setter
+print(p.name)  # Bob
+ 
+
+
+
+
 
 
 
