659b564679ce0a8be6fa1dc25dacae7eaf8c2b1e
sujaydas <sujaydas17595@gmail.com>
2026-08-05 00:07:32 +0530

    incap in python

--- advance concept of python/Static & Class Methods.py
+++ advance concept of python/Static & Class Methods.py
@@ -1,1 +1,10 @@
-print("sujay is great")
+# print("sujay is great")
+class employee:
+    campany ='hp'
+    def __init__(self,name,salary):
+        self.name=name
+        self.salary=salary
+e1 =employee("sujay",100000)
+e2 =employee("rajat",100000)
+
+print(employee.campany)
