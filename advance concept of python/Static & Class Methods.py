12c0ab9ea8e03037da1c085141a89cdd9ce06d75
sujaydas <sujaydas17595@gmail.com>
2026-08-06 21:58:16 +0530

    instence method in python

--- advance concept of python/Static & Class Methods.py
+++ advance concept of python/Static & Class Methods.py
@@ -4,7 +4,15 @@ class employee:
     def __init__(self,name,salary):
         self.name=name
         self.salary=salary
-e1 =employee("sujay",100000)
-e2 =employee("rajat",100000)
+# instance method 
+    def print_info(self):
+        info=f"the name is {self.name} and the salary is {self.salary}"
+        print(info)
 
+e1=employee("sujay",1500000)
+e2=employee("durgesh",78000)
 print(employee.campany)
+# print(employee.name)give the error 
+e1.print_info()
+e2.print_info()
+
