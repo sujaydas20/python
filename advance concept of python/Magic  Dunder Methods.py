812377fce4ac2d300ec91959ff3bd2870fa3864c
sujaydas <sujaydas17595@gmail.com>
2026-08-07 23:17:14 +0530

    Update Magic  Dunder Methods.py

--- advance concept of python/Magic  Dunder Methods.py
+++ advance concept of python/Magic  Dunder Methods.py
@@ -5,8 +5,12 @@ class employee:
         self.salary=salary
     def __str__(self):   #this is dunder method in python
         return f"the name is {self.name}an the salary is {self.salary}"    
-
+    def __repr__(self):
+        return f"name:{self.name }\n"
 e=employee("sujay",150000)        
 print(e.name,e.salary)
 
 print(str(e))
+
+
+print("sujauy")
