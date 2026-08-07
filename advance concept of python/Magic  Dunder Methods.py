766ff7154337ed0f51f72e5dae6af6ebb88e045e
sujaydas <sujaydas17595@gmail.com>
2026-08-07 22:58:32 +0530

    dunder method in python

--- advance concept of python/Magic  Dunder Methods.py
+++ advance concept of python/Magic  Dunder Methods.py
@@ -3,6 +3,10 @@ class employee:
     def __init__(self,name,salary):
         self.name=name
         self.salary=salary
+    def __str__(self):   #this is dunder method in python
+        return f"the name is {self.name}an the salary is {self.salary}"    
 
 e=employee("sujay",150000)        
 print(e.name,e.salary)
+
+print(str(e))
