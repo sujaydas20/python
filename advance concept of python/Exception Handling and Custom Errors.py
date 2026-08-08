48aab8420cfcbe730ba26faebf4e70cb2e696cfc
sujaydas <sujaydas17595@gmail.com>
2026-08-08 23:11:12 +0530

    use the loop in check the exception

--- advance concept of python/Exception Handling and Custom Errors.py
+++ advance concept of python/Exception Handling and Custom Errors.py
@@ -1,3 +1,6 @@
-a=int(input("enter your number 1:  "))
-b=int(input("enter your number2 :   "))
-print(f"the sum of {a+b}")
+
+while True:
+
+    a=int(input("enter your number 1:  "))
+    b=int(input("enter your number2 :   "))
+    print(f"the sum of {a+b}")
