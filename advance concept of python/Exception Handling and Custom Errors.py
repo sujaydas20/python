0e16bbcf9a20269c6e3bc4941251e5f81eb7a4c3
sujaydas <sujaydas17595@gmail.com>
2026-08-08 23:32:07 +0530

    type of excwption

--- advance concept of python/Exception Handling and Custom Errors.py
+++ advance concept of python/Exception Handling and Custom Errors.py
@@ -4,7 +4,12 @@ while True:
 
         a=int(input("enter your number 1:  "))
         b=int(input("enter your number2 :  "))
-        print(f"the sum of {a+b}")
+        print(f"the sum of division is {a/b}")
+
+    except ValueError:
+        print("dont use another value")
+    except ZeroDivisionError:
+        print("please dont use the 0 ")        
 
     except Exception as e:
         print("erroe is found!\ncheck your input",e)
