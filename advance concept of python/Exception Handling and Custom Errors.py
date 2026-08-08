59b2a42e1e1a6e2206b83d69183949d04a988377
sujaydas <sujaydas17595@gmail.com>
2026-08-08 23:24:36 +0530

    using the exception

--- advance concept of python/Exception Handling and Custom Errors.py
+++ advance concept of python/Exception Handling and Custom Errors.py
@@ -3,10 +3,10 @@ while True:
     try:            
 
         a=int(input("enter your number 1:  "))
-        b=int(input("enter your number2 :   "))
+        b=int(input("enter your number2 :  "))
         print(f"the sum of {a+b}")
 
-    except:
-        print("erroe is found!\ncheck your input")
+    except Exception as e:
+        print("erroe is found!\ncheck your input",e)
 
     
