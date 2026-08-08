cc2dfde26f3f90e849cd36910374dbce96c13a20
sujaydas <sujaydas17595@gmail.com>
2026-08-08 23:58:04 +0530

    finally inin exception

--- advance concept of python/else exception.py
+++ advance concept of python/else exception.py
@@ -1,7 +1,33 @@
-try:
-    a=452/5
-except Exception as e:
-    print(e)
-# it check there is no error
+# try:
+#     a=452/5
+# except Exception as e:
+#     print(e)
+# # it check there is no error
 # else:
 #     print("good no error")        
+
+
+
+
+
+
+
+
+
+
+
+
+def divide(a,b):
+    try:
+        c=a/b
+        print(c)
+        return c
+    except Exception as e:
+        print(e)
+        return None
+    # this is always excutedmno for outhers
+    finally:
+        print("this is always excuted")
+a=int(input("enter your number 1:"))      
+b=int(input("enter your number 2:"))   
+divide(a,b)
