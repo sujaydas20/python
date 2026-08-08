8185eb4ab534f531be13fa3982639bd69fcb6b47
sujaydas <sujaydas17595@gmail.com>
2026-08-08 23:41:50 +0530

    raise the error

--- advance concept of python/Exception Handling and Custom Errors.py
+++ advance concept of python/Exception Handling and Custom Errors.py
@@ -1,17 +1,27 @@
 
+# while True:
+#     try:            
+
+#         a=int(input("enter your number 1:  "))
+#         b=int(input("enter your number2 :  "))
+#         print(f"the sum of division is {a/b}")
+
+#     except ValueError:
+#         print("dont use another value")
+#     except ZeroDivisionError:
+#         print("please dont use the 0 ")        
+
+#     except Exception as e:
+#         print("erroe is found!\ncheck your input",e)
+
+
 while True:
-    try:            
+    a=int(input("enter your number 1:  "))
+    b=int(input("enter your number2 :  ")) 
 
-        a=int(input("enter your number 1:  "))
-        b=int(input("enter your number2 :  "))
-        print(f"the sum of division is {a/b}")
 
-    except ValueError:
-        print("dont use another value")
-    except ZeroDivisionError:
-        print("please dont use the 0 ")        
+    if b==0:
+        raise ValueError("sont use 0")
+    print(f"the sum of division is {a/b}")
 
-    except Exception as e:
-        print("erroe is found!\ncheck your input",e)
-
-    
+ 
