5c42378ad9f7d6062a0b2bbf15fbb39cecadbe83
sujaydas <sujaydas17595@gmail.com>
2026-08-08 23:20:18 +0530

    try and except handling in python

--- advance concept of python/Exception Handling and Custom Errors.py
+++ advance concept of python/Exception Handling and Custom Errors.py
@@ -1,6 +1,12 @@
 
 while True:
+    try:            
 
-    a=int(input("enter your number 1:  "))
-    b=int(input("enter your number2 :   "))
-    print(f"the sum of {a+b}")
+        a=int(input("enter your number 1:  "))
+        b=int(input("enter your number2 :   "))
+        print(f"the sum of {a+b}")
+
+    except:
+        print("erroe is found!\ncheck your input")
+
+    
