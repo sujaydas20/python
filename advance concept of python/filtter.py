4c30e40fcaaefd0053e6303aef7d2dd76a716137
sujaydas <sujaydas17595@gmail.com>
2026-08-12 21:46:46 +0530

    filtter in  python

---
+++ advance concept of python/filtter.py
@@ -0,0 +1,10 @@
+def greater_than_9(x):
+    if x>9:
+        return True
+    else:
+        return False
+
+
+a=[1,3,4,5,6,55,268,58,9,55,66,99,77,100,2225]    
+new=  list(filter(greater_than_9,a))
+print(new)
