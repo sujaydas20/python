d63ebb01cec4c4e1cff43fc741e9ead007da911a
sujaydas <sujaydas17595@gmail.com>
2026-08-12 21:25:44 +0530

    use loop to run the program untill condition  is correct

--- advance concept of python/Exception Handling .py
+++ advance concept of python/Exception Handling .py
@@ -1,3 +1,11 @@
-a=int(input("enter your number1 "))
-b=int(input("enter your number2 "))
-print(f"the sum of this number{a+b}")
+
+
+
+while True:
+
+
+     a=int(input("enter your number1:- "))
+     b=int(input("enter your number2:- "))
+
+     print(f"the sum of this number{a+b}")
+
