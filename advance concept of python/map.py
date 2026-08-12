e6652fb5c1337a1c1e32394b02e5bd691e3b7985
sujaydas <sujaydas17595@gmail.com>
2026-08-12 21:50:11 +0530

    lambda in map or python

--- advance concept of python/map.py
+++ advance concept of python/map.py
@@ -6,3 +6,19 @@ def squere(x):
 
 new =list (map(squere,num))
 print(new)
+
+
+
+
+
+
+
+
+num=[1,2,3,45,4,21]
+
+# def squere(x):
+#     return x*x
+
+
+new =list (map(lambda x:x*x,num))
+print(new)
