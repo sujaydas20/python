61f72bcb3d8c33a0bd990ff52cfa4fdd0569b945
sujaydas <sujaydas17595@gmail.com>
2026-08-12 21:52:46 +0530

    lambda if python

--- advance concept of python/filtter.py
+++ advance concept of python/filtter.py
@@ -8,3 +8,19 @@ def greater_than_9(x):
 a=[1,3,4,5,6,55,268,58,9,55,66,99,77,100,2225]    
 new=  list(filter(greater_than_9,a))
 print(new)
+
+
+
+
+
+
+# def greater_than_9(x):
+#     if x>9:
+#         return True
+#     else:
+#         return False
+
+
+a=[1,3,4,5,6,55,268,58,9,55,66,99,77,100,2225]    
+new=  list(filter(lambda x:x>9,a))
+print(new)
