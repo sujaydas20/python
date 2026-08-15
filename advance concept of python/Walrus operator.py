93a13b2a01acdad42bfa29654718fc0ae3a788d7
sujaydas <sujaydas17595@gmail.com>
2026-08-15 23:04:22 +0530

    add in walrsu operator

--- advance concept of python/Walrus operator.py
+++ advance concept of python/Walrus operator.py
@@ -1,5 +1,23 @@
-while(data:=input("enter your nuber")):
-    print(data)
-    if (data) =="x":
-        print(data+data)
-        break      
+# while(data:=input("enter your nuber")):
+#     print(data)
+#     if (data) =="x":
+#         print()
+#         break      
+
+
+
+
+
+
+
+
+
+total = 0
+
+while (data := input("Enter your number (x to stop): ")):
+    if data == "x":
+        break
+
+    total += int(data)
+
+print("Total =", total)
