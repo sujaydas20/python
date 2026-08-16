ca109483fef2963d54c24b87416b8dd534042e44
sujaydas <sujaydas17595@gmail.com>
2026-08-16 22:05:34 +0530

    Args in python

--- advance concept of python/Args and kwargs.py
+++ advance concept of python/Args and kwargs.py
@@ -1,3 +1,16 @@
-def sum(a,b):
-    return a+b
-print(sum(555,22))
+# def sum(a,b):
+#     return a+b
+# print(sum(555,22))
+
+
+
+
+
+def sum(*args):
+    total=0
+    for item in args:
+        total+=item
+    return total
+
+
+print(sum(444,888,999,666))
